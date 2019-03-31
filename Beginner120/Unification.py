17be6527f82a56b60340b70309e3bb07b0221a5c
yasunari89 <yasunari89@gmail.com>
2019-04-01 05:04:58 +0900

    apply tips

--- Beginner120/Unification.py
+++ Beginner120/Unification.py
@@ -1,17 +1,5 @@
-S = list(input())
-N = len(S)
-
-counter = 0
-i = 0
-while(True):
-    if not '0' in S or not '1' in S or S == []:
-        break
-    elif (S[i] == '1' and S[i+1] == '0') or (S[i] == '0' and S[i+1] == '1'):
-        S.pop(i)
-        S.pop(i)
-        counter += 2
-        i -= 1
-    else:
-        i += 1
-
-print(counter)
+S = input()
+count0 = S.count('0')
+count1 = S.count('1')
+ans = 2 * min(count0, count1)
+print(ans)
