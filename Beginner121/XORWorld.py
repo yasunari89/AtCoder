664cd76e13b3e5a898356439284ce61603b96915
yasunari89 <yasunari89@gmail.com>
2019-04-01 01:15:49 +0900

    change codes with the features of XOR

--- Beginner121/XORWorld.py
+++ Beginner121/XORWorld.py
@@ -1,50 +1,21 @@
 A, B = map(int, input().split())
 
-summary = A
-for XORed in range(A+1, B+1):
-    summary = summary ^ XORed
+# summary = A
+# for XORed in range(A+1, B+1):
+#     summary = summary ^ XORed
 
-print(summary)
+def XORfromZERO(x):
+    rest = x % 2
+    quotient = (x + rest) / 2
+    if rest == 0:
+        if quotient % 2 == 0:
+            return 0 ^ x
+        else:
+            return 1 ^ x
+    else:
+        if quotient % 2 == 0:
+            return 0
+        else:
+            return 1
 
-# def int2bin(x):
-#     quotient = x
-#     string_bin = ''
-#     while(quotient != 1):
-#         rest = quotient % 2
-#         quotient = int((quotient - rest) / 2)
-#         string_bin = str(rest) + string_bin
-#     string_bin = '1' + string_bin
-#     return int(string_bin)
-#
-# def bin2int(x):
-#     power = int(len(str(x)) - 1)
-#     integer = 0
-#     i = 0
-#     while(power >= 0):
-#         integer += 2**power * int(str(x)[i])
-#         i += 1
-#         power -= 1
-#     return integer
-
-
-# divided_bin_str1 = str(int2bin(A))
-# for divided in range(A+1, B+1):
-#     added = ''
-#     divided_bin_str2 = str(int2bin(divided))
-#     if len(divided_bin_str1) < len(divided_bin_str2):
-#         divided_bin_str1 = '0' + divided_bin_str1
-#     else:
-#         pass
-#     # print(divided_bin_str1, divided_bin_str2)
-#     for i in range(len(divided_bin_str1)):
-#         ans = int(divided_bin_str1[i]) + int(divided_bin_str2[i])
-#         if ans == 1:
-#             added += '1'
-#         else:
-#             added += '0'
-#     divided_bin_str1 = added
-#
-# added = int(added)
-# ans = bin2int(added)
-#
-# print(ans)
+print(XORfromZERO(A-1) ^ XORfromZERO(B))
