46d7967039db3b771d037737c94bbe5a2be30d91
yasunari89 <yasunari89@gmail.com>
2019-04-01 22:22:46 +0900

    add xortool

--- Tools.py
+++ Tools.py
@@ -1,9 +1,12 @@
+from enum import Enum
+
+
 def maxDivisor(divisor, divided):
     '''最大公約数を返す関数'''
     if divisor > divided:
-        print("Error: 引数の順番が逆！")
+        print(Errors.reverse_variables_error.value)
     elif type(divisor) != int or type(divided) != int:
-        print("Error: 引数はint型に！")
+        print(Errors.not_int_error.value)
     else:
         while(True):
             rest = divided % divisor
@@ -13,10 +16,11 @@ def maxDivisor(divisor, divided):
                 divided = divisor
                 divisor = rest
 
+
 def divisors(x):
     '''公約数をリスト型で返す関数'''
     if type(x) != int:
-        print("Error: 引数はint型に！")
+        print(Errors.not_int_error.value)
     else:
         divisors = []
         divisor = 1
@@ -31,3 +35,32 @@ def divisors(x):
                 pass
             divisor += 1
         return sorted(divisors)
+
+
+def XORfromZERO(x):
+    '''
+    Σ[0-(x-1)]Π(n xor (n+1))を返す関数
+    つまり0からxまでの全ての数をXORする関数
+    '''
+    if type(x) != int:
+        print(Errors.not_int_error.value)
+    rest = x % 2
+    quotient = (x + rest) / 2
+    if rest == 0:
+        if quotient % 2 == 0:
+            return 0 ^ x
+        else:
+            return 1 ^ x
+    else:
+        if quotient % 2 == 0:
+            return 0
+        else:
+            return 1
+
+
+
+
+
+class Errors(Enum):
+    reverse_variables_error = "Error: 引数の順番が逆！"
+    not_int_error = "Error: 引数はint型に！"
