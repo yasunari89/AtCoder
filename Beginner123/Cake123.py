1fcd7d952612393f9e2529dc5ce8d55790d798d7
yasunari89 <yasunari89@gmail.com>
2019-04-07 19:51:10 +0900

    add Cake123

--- Beginner123/Cake123.py
+++ Beginner123/Cake123.py
@@ -1,28 +1,20 @@
 X, Y, Z, K = map(int, input().split())
-A = sorted(list(map(int, input().split())))
-B = sorted(list(map(int, input().split())))
-C = sorted(list(map(int, input().split())))
-all = sorted(A + B + C)
+A = sorted(list(map(int, input().split())), reverse=True)
+B = sorted(list(map(int, input().split())), reverse=True)
+C = sorted(list(map(int, input().split())), reverse=True)
 
-indices = [-1, -1, -1]
+ABC = []
+for a in range(X):
+    for b in range(Y):
+        if (a+1) * (b+1) > K:
+            break
+        else:
+            pass
+        for c in range(Z):
+            if (a+1) * (b+1) * (c+1) > K:
+                break
+            else:
+                ABC.append(A[a] + B[b] + C[c])
 
-def sumABC(a, b, c):
-    return A[indices[0]-a] + B[indices[1]-b] + C[indices[2]-c]
-
-print("answers")
-i = 0
-while(i < K):
-    ret0 = sumABC(0, 0, 0)
-    ret1 = sumABC(1, 0, 0)
-    ret2 = sumABC(0, 1, 0)
-    ret3 = sumABC(0, 0, 1)
-    ret4 = sumABC(1, 1, 0)
-    ret5 = sumABC(1, 0, 1)
-    ret6 = sumABC(0, 1, 1)
-    ret7 = sumABC(1, 1, 1)
-    returns = sorted([ret0, ret1, ret2, ret3, ret4, ret5, ret6, ret7])
-    for j in range(7, -1, -1):
-        i += 1
-        print(returns[j])
-    for _ in range(3):
-        indices[_] += -1
+for i in range(K):
+    print(sorted(ABC, reverse=True)[i])
