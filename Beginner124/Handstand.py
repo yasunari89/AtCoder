d3c304aed716d4b7ac6fc778ac36d4572490d98d
yasunari89 <yasunari89@gmail.com>
2019-04-16 17:41:30 +0900

    compelte handstand

--- Beginner124/Handstand.py
+++ Beginner124/Handstand.py
@@ -17,6 +17,8 @@ nums.append(cnt)
 if int(S[-1]) == 0:
     nums.append(0)
 
+# print(nums)
+
 cumulative_sum = []
 M = len(nums)
 sum = 0
@@ -24,13 +26,19 @@ for i in range(M):
     sum += nums[i]
     cumulative_sum.append(sum)
 
+# print(cumulative_sum)
+
 ans = 0
 start = 0
 final = 2 * K
 answers = []
 
-if final < M:
-    while(final < M):
+# M = len(nums) = len(cumulative_sum)
+if M < 2 * K + 1:
+    print(len(S))
+    exit()
+if final <= M-1:
+    while(final <= M-1):
         if start == 0:
             answers.append(cumulative_sum[final])
         else:
@@ -43,3 +51,5 @@ if final < M:
     print(max(answers))
 else:
     print(cumulative_sum[0])
+
+# print(answers)
