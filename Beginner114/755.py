aa35fb70fb1bd9d285cc40f7055bf359ef46f280
yasunari89 <yasunari89@gmail.com>
2019-04-22 17:58:01 +0900

    add 755

--- Beginner114/755.py
+++ Beginner114/755.py
@@ -3,10 +3,9 @@ N = int(input())
 def dfs(s):
     if int(s) > N:
         return 0
-    else:
-        ret = 1 if all(s.count(c) > 0 for c in '753') else 0
-        for c in '753':
-            ret = dfs(s + c)
-        return ret
+    ret = 1 if all(s.count(c) > 0 for c in '753') else 0
+    for c in '753':
+        ret += dfs(s + c)
+    return ret
 
 print(dfs('0'))
