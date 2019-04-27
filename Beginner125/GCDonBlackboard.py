c35f261e5bcfe9d4fc5efb8c34c93c6301322ed2
yasunari89 <yasunari89@gmail.com>
2019-04-27 22:45:06 +0900

    add

--- Beginner125/GCDonBlackboard.py
+++ Beginner125/GCDonBlackboard.py
@@ -1,4 +1,5 @@
 import fractions as f
+import heapq
 N = int(input())
 As = list(map(int, input().split()))
 ans = 1
@@ -6,10 +7,11 @@ ans = 1
 for i in range(N):
     new_As = As.copy()
     new_As.pop(i)
-    new_As = sorted(new_As)
+    # new_As = sorted(new_As)
+    mins = heapq.nsmallest(2, new_As)
     if len(new_As) == 1:
-        ans = max(ans, new_As[1])
+        ans = max(ans, mins[0])
     else:
-        ans = max(ans, f.gcd(new_As[0], new_As[1]))
+        ans = max(ans, f.gcd(mins[0], mins[1]))
 
 print(ans)
