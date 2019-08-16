4fd4644ad64da2e56cd4e12887a2b1463eee4602
yasunari89 <yasunari89@gmail.com>
2019-08-16 11:25:35 +0900

    execute mypy

--- probs/ABC134/exception_handling.py
+++ probs/ABC134/exception_handling.py
@@ -1,11 +1,14 @@
-def main():
-    N = int(input())
-    A = [int(input()) for _ in range(N)]
-    current_max = max(A[1:N])
-    current_max_index = A.index(current_max)
+from typing import List
+
+
+def main() -> None:
+    N: int = int(input())
+    A: List[int] = [int(input()) for _ in range(N)]
+    current_max: int = max(A[1:N])
+    current_max_index: int = A.index(current_max)
     print(current_max)
-    previous_max = current_max
-    previous_max_index = current_max_index
+    previous_max: int = current_max
+    previous_max_index: int = current_max_index
     for i in range(1, N):
         if i != previous_max_index:
             current_max = max([A[i-1], previous_max])
