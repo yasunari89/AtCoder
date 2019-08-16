35bf50e53b40bcbba5c1e6566648930148b57853
yasunari89 <yasunari89@gmail.com>
2019-08-16 10:25:23 +0900

    comment tips

--- probs/ABC136/build_stairs.py
+++ probs/ABC136/build_stairs.py
@@ -4,6 +4,8 @@ N: int = int(input())
 H: List[int] = list(map(int, input().split()))
 
 exit_flag: bool = False
+# 操作が「小さくする」なので走査もreverseして
+# 小さいかどうかを調べるようにする
 for i in range(N-1, 0, -1):
     if H[i-1] - H[i] == 1:
         H[i-1] -= 1
