c890d259e3bc7a97de1cfb51be79ea1ec36567b7
yasunari89 <yasunari89@gmail.com>
2020-07-11 19:16:48 +0900

    update dfs

--- typical-contest-001/dfs.py
+++ typical-contest-001/dfs.py
@@ -6,7 +6,27 @@ for _ in range(height):
     row = [l for l in input()]
     maze.append(row)
 
+class Start:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+class Goal:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+start = Start()
+goal = Goal()
+for x in range(height):
+    for y in range(width):
+        if maze[x][y] == 's':
+            start.x, start.y = x, y
+        if maze[x][y] == 'g':
+            goal.x, goal.y = x, y
+
+# Fill-out DFS
 reached = [[False for _ in range(width)] for _ in range(height)]
+
 def search(x, y):
     if x < 0 or y < 0:
         return 
@@ -16,12 +36,14 @@ def search(x, y):
         return
     if reached[x][y] == True:
         return 
-    
     reached[x][y] = True
     search(x+1, y)
     search(x, y+1)
     search(x-1, y)
     search(x, y-1)
 
-search(0, 0)
-pprint(reached)
+search(start.x, start.y)
+if reached[goal.x][goal.y] == True:
+    print('Yes')
+else:
+    print('No')
