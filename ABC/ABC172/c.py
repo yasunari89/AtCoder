59ddf33375188a46f31f8c9f8f4d6f395163052a
yasunari89 <yasunari89@gmail.com>
2020-07-19 21:22:31 +0900

    update

--- ABC/ABC172/c.py
+++ ABC/ABC172/c.py
@@ -1,22 +1,27 @@
-alen, blen, time = map(int, input().split())
-abooks = list(map(int, input().split()))
-bbooks = list(map(int, input().split()))
+n, m, k = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
-counter = 0
-reading_time = 0
-while time > 0:
-    if len(abooks) < 1 and len(bbooks) < 1:
-        break
-    elif len(abooks) < 1 and len(bbooks) > 0:
-        reading_time = bbooks.pop(0)
-    elif len(abooks) > 0 and len(bbooks) < 1:
-        reading_time = abooks.pop(0)
-    elif abooks[0] < bbooks[0]:
-        reading_time = abooks.pop(0)
-    else:
-        reading_time = bbooks.pop(0)
-    time -= reading_time
-    if time >= 0:
-        counter += 1
+a = [0]
+b = [0]
+total = 0
+for Ai in A:
+    total += Ai
+    a.append(total)
+total = 0
+for Bi in B:
+    total += Bi
+    b.append(total)
 
-print(counter)
+j = len(b) - 1
+ans = 0
+for i in range(len(a)):
+    if a[i] > k:
+        continue
+    while a[i] + b[j] > k:
+        j -= 1
+        if j < 0:
+            break
+    ans = max(ans, i + j)
+
+print(ans)
